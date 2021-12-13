3b15b658fa684bbbaf625496116a6d7638d0ae4a
wheelchairdom <77422094+wheelchairdom@users.noreply.github.com>
2021-12-13 10:35:47 -1000

    step 3 framework

--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@ mymenubutton.onclick = function () {
 
     }
 };
+
+// REVEAL ON SCROLL JS
+// CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+const myobserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.setAttribute('data-sectionstate', 'active');
+        } else {
+            entry.target.setAttribute('data-sectionstate', 'inactive');
+        }
+    });
+});
+document.querySelectorAll('.animate-on-scroll').forEach((section) => {
+    myobserver.observe(section);
+});
